1fe793723378f34c9290c786aed66b478a1b8183
JWilhelm <janmarco.wilhelm@uzh.ch>
2020-02-02 00:41:47 +0100

    also 10_Na6 bigger box

--- 4_calcs_daint/02_RI-5Z_Pade_16_Nminimax_14_NClens_150_N_Sigma_150_FLO_0.2eV_wmax_1.0/submit_script.sh
+++ 4_calcs_daint/02_RI-5Z_Pade_16_Nminimax_14_NClens_150_N_Sigma_150_FLO_0.2eV_wmax_1.0/submit_script.sh
@@ -25,9 +25,13 @@ do
   cp $xyzfile .
 
   cp $calcdir'/'$submitfile .
-  if [ $dirname == '23_C4H10' ] || [ $dirname == '28_C6H6' ] || [ $dirname == '29_C8H8' ] || [ $dirname == '30_C5H6' ] || [ $dirname == '37_CBr4' ] || [ $dirname == '38_CI4' ] || [ $dirname == '42_Si5H12' ] || [ $dirname == '56_TiF4' ] || [ $dirname == '64_AlI3' ] || [ $dirname == '73_C4H10O4' ] || [ $dirname == '86_C7H8' ] || [ $dirname == '87_C8H10' ] || [ $dirname == '88_C6F6' ] || [ $dirname == '89_C6H5OH' ] || [ $dirname == '90_C6H5NH2' ] || [ $dirname == '91_C5H5N' ] || [ $dirname == '92_guanine' ] || [ $dirname == '93_adenine' ] || [ $dirname == '94_cytosine' ] || [ $dirname == '95_thymine' ] || [ $dirname == '96_uracil' ] ; then
+  if [ $dirname == '23_C4H10' ] || [$dirname == '27_C3H6'] || [ $dirname == '28_C6H6' ] || [ $dirname == '29_C8H8' ] || [ $dirname == '30_C5H6' ] || [ $dirname == '37_CBr4' ] || [ $dirname == '42_Si5H12' ] || [ $dirname == '56_TiF4' ] || [ $dirname == '64_AlI3' ] || [ $dirname == '73_C4H10O4' ] || [ $dirname == '86_C7H8' ] || [ $dirname == '87_C8H10' ] || [ $dirname == '88_C6F6' ] || [ $dirname == '89_C6H5OH' ] || [ $dirname == '90_C6H5NH2' ] || [ $dirname == '91_C5H5N' ] || [ $dirname == '92_guanine' ] || [ $dirname == '93_adenine' ] || [ $dirname == '94_cytosine' ] || [ $dirname == '95_thymine' ] || [ $dirname == '96_uracil' ] ; then
 
     sed -i -e 's/<nodes>/8/g' $submitfile
+
+  elif [ $dirname == '38_CI4' ] ; then
+
+    sed -i -e 's/<nodes>/16/g' $submitfile
 
   else
 
@@ -35,16 +39,20 @@ do
 
   fi
 
-  if [ $dirname == '56_TiF4' ] || [ $dirname == '65_BN' ] || [ $dirname == '67_PN' ] || [ $dirname == '84_BeO' ] || [ $dirname == '85_MgO' ]  ; then
+  if [ $dirname == '56_TiF4' ] || [ $dirname == '65_BN' ] || [ $dirname == '67_PN' ] || [ $dirname == '81_CO' ] || [ $dirname == '84_BeO' ] || [ $dirname == '85_MgO' ]  ; then
       cp $wfndir'/'$dirname'/ALL_ELEC-RESTART.wfn' .
       echo 'Copied WFN for   '$xyzname'.'
+  fi
+
+  if [ $dirname == '42_Si5H12' ] || [$dirname == '10_Na6'] ; then
+      sed -i -e 's/ABC 16.0 16.0 16.0/ABC 16.0 26.0 16.0/g' $GWfile
   fi
 
   if grep -Fq "( vir )" $cp2koutfile
   then
       echo 'Calculation for  '$xyzname' already terminated. -> Skip it.'
   else
-      sbatch $submitfile
+#      sbatch $submitfile
       echo 'Started calc for '$xyzname'.'
   fi
 
